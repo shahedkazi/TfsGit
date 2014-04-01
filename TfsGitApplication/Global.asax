9d712b09c25882364477effb3e5e084b69e4a53a
shahed.kazi <shahedjamil@hotmail.com>
2014-04-01 15:53:48 +1100

    Initial Load

---
+++ TfsGitApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TfsGitApplication.MvcApplication" Language="C#" %>
---
+++ TfsGitApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TfsGitApplication.Startup))]
+namespace TfsGitApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
